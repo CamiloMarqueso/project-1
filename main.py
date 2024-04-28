12b2d2741b8b6b57020c4820f497ab3c76f4beb9
CamiloMarqueso <168359760+CamiloMarqueso@users.noreply.github.com>
2024-04-28 11:19:26 -0500

    Create main.py
    projecto

---
+++ main.py
@@ -0,0 +1,3 @@
+import random
+datos = ["me gusta jugar videojuegos", "he tenido muchas mascotas como tortugas y conejos", "estoy orgulloso del arte de pixelart que hice", "mi comida favorita es una comida que hace mi papá llamado wrap"]
+print(random.choice(datos))
